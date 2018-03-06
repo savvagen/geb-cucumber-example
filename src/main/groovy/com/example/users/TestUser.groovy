becba38a1121c5b96541e6a1a9fa061957efbea3
Savva Genchevskiy <sgenchevskiy@electric-cloud.com>
2018-03-06 17:08:29 +0200

    stable version

---
+++ src/main/groovy/com/example/users/TestUser.groovy
@@ -0,0 +1,15 @@
+package com.example.users
+
+class TestUser {
+
+
+    String email
+    String password
+
+    TestUser(String email = "genchevskiy.test@gmail.com", String password = "s.g19021992"){
+        this.email = email
+        this.password = password
+    }
+
+
+}
