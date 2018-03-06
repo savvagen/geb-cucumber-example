0b514174627a19d12c5c5e8da892a981cecacbe2
Savva Genchevskiy <sgenchevskiy@electric-cloud.com>
2018-03-06 17:15:32 +0200

    stable version

--- src/test/groovy/com/example/ApiTests/ApiExampleTests.groovy
+++ src/test/groovy/com/example/ApiTests/ApiExampleTests.groovy
@@ -4,6 +4,7 @@ import com.example.models.User
 import com.example.models.UserStatus
 import groovy.json.JsonBuilder
 import groovyx.net.http.HTTPBuilder
+import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 
 import static groovyx.net.http.ContentType.*
@@ -14,18 +15,32 @@ class ApiExampleTests {
 
     def http = new HTTPBuilder()
 
+    User user
+    String baseUri = "http://localhost:8087"
+
+    @BeforeClass
+    void setUpTests(){
+        List<String> phones = new ArrayList<>()
+        phones.add("+380995475717")
+        phones.add("+380995475718")
+        user = new User(Long.valueOf(1), "Test",
+                "TestUser",
+                "genchevskiy.test@gmail.com",
+                "s.g19021992", phones)
+    }
+
 
     @Test
     void getUser(){
-        http.request("http://localhost:8087/users/1", GET, JSON){ request ->
-            headers.Accept = 'application/json'
+        http.request("$baseUri/users/1", GET, JSON){ request ->
+            headers.Accept = JSON
             response.success = { resp, json ->
                 assert resp.statusLine.statusCode == 200
                 assert json.userId == 1
                 assert json.phones.size() == 2
                 println "Got response: ${resp.statusLine}"
                 println "Content-Type: ${resp.headers.'Content-Type'}"
-                println new JsonBuilder(json)
+                println new JsonBuilder(json).toPrettyString()
                 println json.userId
                 println json.phones[0]
                 User client = json.asType(User)
@@ -36,12 +51,8 @@ class ApiExampleTests {
 
     @Test
     void postUser() {
-        http.request("http://localhost:8087/users/register", POST, JSON) { request ->
-            List<String> phones = new ArrayList<>()
-            phones.add("+380995475717")
-            phones.add("+380995475718")
-            body = new User(Long.valueOf("1"), "Test", "TestUser", "genchevskiy.test@gmail.com", "s.g19021992", phones)
-            //body = ["userId":1,"firstName":"Test","lastName":"TestUser","email":"genchevskiy.test@gmail.com", "password": "s.g19021992", "phones":["+380995475717","+380995475718"]]
+        http.request("$baseUri/users/register", POST, JSON) { request ->
+            body = user
             response.success = { resp, json ->
                 assert resp.statusLine.statusCode == 200
                 assert json.status == "success"
